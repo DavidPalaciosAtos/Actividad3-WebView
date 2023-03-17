8ea3c78e08ae0dab08c6558e0f8056cfa08ac860
DavidPalaciosAtos <david.palacios.external@atos.net>
2023-03-17 09:24:29 +0100

    Initial commit

--- app/src/main/java/com/example/actividad3_webview/MainActivity.java
+++ app/src/main/java/com/example/actividad3_webview/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.actividad3_webview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.example.actividad3_webview.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding enlace;
+
+    WebView wv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        enlace = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(enlace.getRoot());
+
+        wv1 = enlace.WebView;
+
+        wv1.setWebViewClient(new WebViewClient());
+        wv1.loadUrl("https://www.google.es/");
+
     }
 }
